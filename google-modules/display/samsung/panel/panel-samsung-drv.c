d3c83712729c7cb2268a55aa3b4a6832ce91e6b4
Sultan Alsawaf <sultan@kerneltoast.com>
2023-04-11 23:12:45 -0700

    drm: samsung: Fix compilation when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/display/samsung/panel/panel-samsung-drv.c
+++ google-modules/display/samsung/panel/panel-samsung-drv.c
@@ -3062,6 +3062,7 @@ static int panel_debugfs_add(struct exynos_panel *ctx, struct dentry *parent)
 
 	return 0;
 }
+#endif
 
 static ssize_t exynos_dsi_dcs_transfer(struct mipi_dsi_device *dsi, u8 type,
 				     const void *data, size_t len, u16 flags)
@@ -3123,6 +3124,7 @@ ssize_t exynos_dsi_dcs_write_buffer(struct mipi_dsi_device *dsi,
 }
 EXPORT_SYMBOL_GPL(exynos_dsi_dcs_write_buffer);
 
+#ifdef CONFIG_DEBUG_FS
 static int exynos_dsi_name_show(struct seq_file *m, void *data)
 {
 	struct mipi_dsi_device *dsi = m->private;
@@ -3133,6 +3135,7 @@ static int exynos_dsi_name_show(struct seq_file *m, void *data)
 	return 0;
 }
 DEFINE_SHOW_ATTRIBUTE(exynos_dsi_name);
+#endif
 
 static ssize_t parse_byte_buf(u8 *out, size_t len, char *src)
 {
@@ -3174,6 +3177,7 @@ static ssize_t exynos_panel_parse_byte_buf(char *input_str, size_t input_len,
 	return rc;
 }
 
+#ifdef CONFIG_DEBUG_FS
 struct exynos_dsi_reg_data {
 	struct mipi_dsi_device *dsi;
 	u8 address;
