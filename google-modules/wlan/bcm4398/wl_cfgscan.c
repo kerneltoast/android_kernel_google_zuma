3f38992c935b3ce52e7f88043d191afe0393286d
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-21 12:15:22 -0700

    bcmdhd: Remove errant preempt disables from wl_cfgscan callbacks
    These preempt disables appear to have no functional purpose besides causing
    mischief on RT:
    [   24.838130] BUG: sleeping function called from invalid context at kernel/locking/rtmutex.c:970
    [   24.838133] in_atomic(): 1, irqs_disabled(): 0, non_block: 0, pid: 3330, name: kworker/u17:3
    [   24.838138] Preemption disabled at:
    [   24.838139] [<ffffffdc87e5cf00>] wl_inform_bss+0x40/0x280
    [   24.838155] CPU: 3 PID: 3330 Comm: kworker/u17:3 Tainted: G        W         5.10.149-rt74-Sultan #1
    [   24.838161] Hardware name: GS201 CHEETAH MP based on GS201 (DT)
    [   24.838164] Workqueue: dhd_eventd wl_event_handler
    [   24.838174] Call trace:
    [   24.838175] dump_backtrace+0x0/0x1a0
    [   24.838184] show_stack+0x14/0x20
    [   24.838189] dump_stack_lvl+0xc0/0xe4
    [   24.838197] dump_stack+0x14/0x4c
    [   24.838201] ___might_sleep+0x138/0x188
    [   24.838209] rt_spin_lock+0x24/0x88
    [   24.838214] cfg80211_bss_update+0x48/0x5e8
    [   24.838222] cfg80211_inform_bss_frame_data+0x1bc/0x6b4
    [   24.838227] wl_inform_single_bss+0x310/0x183c
    [   24.838233] wl_inform_bss+0x98/0x280
    [   24.838238] wl_escan_handler+0xa30/0x2660
    [   24.838245] wl_event_handler+0x5cc/0x8a0
    [   24.838250] process_one_work+0x1a8/0x360
    [   24.838257] worker_thread+0x6c/0x44c
    [   24.838261] kthread+0x18c/0x190
    [   24.838266] ret_from_fork+0x10/0x24
    [   24.838486] ------------[ cut here ]------------
    
    Remove them since they're not used anywhere else in bcmdhd and appear to be
    useless.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/wlan/bcm4398/wl_cfgscan.c
+++ google-modules/wlan/bcm4398/wl_cfgscan.c
@@ -534,7 +534,6 @@ wl_inform_bss(struct bcm_cfg80211 *cfg)
 #ifdef ESCAN_CHANNEL_CACHE
 	reset_roam_cache(cfg);
 #endif /* ESCAN_CHANNEL_CACHE */
-	preempt_disable();
 	bi = next_bss(bss_list, bi);
 	for_each_bss(bss_list, bi, i) {
 #ifdef ESCAN_CHANNEL_CACHE
@@ -545,7 +544,6 @@ wl_inform_bss(struct bcm_cfg80211 *cfg)
 			WL_ERR(("bss inform failed\n"));
 		}
 	}
-	preempt_enable();
 	WL_MEM(("cfg80211 scan cache updated\n"));
 #if defined(ROAM_CHANNEL_CACHE) && defined(HOST_RCC_UPDATE)
 	/* print_roam_cache(); */
@@ -1302,7 +1300,6 @@ s32 wl_cfgscan_pfn_handler(struct bcm_cfg80211 *cfg, wl_pfn_scanresult_v3_1_t *p
 		return -EINVAL;
 	}
 
-	preempt_disable();
 #ifdef ESCAN_CHANNEL_CACHE
 	add_roam_cache(cfg, bi);
 #endif /* ESCAN_CHANNEL_CACHE */
@@ -1310,7 +1307,6 @@ s32 wl_cfgscan_pfn_handler(struct bcm_cfg80211 *cfg, wl_pfn_scanresult_v3_1_t *p
 	if (unlikely(err)) {
 		WL_ERR(("bss inform failed\n"));
 	}
-	preempt_enable();
 	WL_MEM(("cfg80211 scan cache updated\n"));
 exit:
 	return err;
