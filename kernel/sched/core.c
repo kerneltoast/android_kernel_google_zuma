f7890c1f6cf2ac75465319b3c4f216510b497e80
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-26 12:31:46 -0700

    sched/core: Panic when a DEBUG_ATOMIC_SLEEP warning gets tripped
    The possibility of sleeping in atomic context is something that we want to
    know when debugging for atomic sleeps. Panic when this occurs so such
    issues can be detected more easily.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9811,6 +9811,7 @@ void ___might_sleep(const char *file, int line, int preempt_offset)
 
 	trace_android_rvh_schedule_bug(NULL);
 
+	panic("%s: Panicking due to potential bug", __func__);
 	dump_stack();
 	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
 }
@@ -9839,6 +9840,7 @@ void __cant_sleep(const char *file, int line, int preempt_offset)
 			current->pid, current->comm);
 
 	debug_show_held_locks(current);
+	panic("%s: Panicking due to potential bug", __func__);
 	dump_stack();
 	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
 }
