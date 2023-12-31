21a986b92302d290718e062c73b18372f5deac38
Sultan Alsawaf <sultan@kerneltoast.com>
2023-12-30 19:19:44 -0800

    lwis: Disable IRQ affinity management when SBalance is enabled
    Let SBalance handle IRQ affinities when it's enabled for better efficiency
    and performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/lwis/platform/casablanca/lwis_platform_casablanca.c
+++ google-modules/lwis/platform/casablanca/lwis_platform_casablanca.c
@@ -410,5 +410,9 @@ int lwis_platform_update_bts(struct lwis_device *lwis_dev, int block, unsigned i
 int lwis_plaform_set_default_irq_affinity(unsigned int irq)
 {
 	const int cpu = 0x2;
+
+	if (IS_ENABLED(CONFIG_IRQ_SBALANCE))
+		return 0;
+
 	return irq_set_affinity_hint(irq, cpumask_of(cpu));
 }
