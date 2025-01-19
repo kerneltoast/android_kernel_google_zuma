e5b3149c2e4507b5f5aa3b2cf964753c3220e300
Sultan Alsawaf <sultan@kerneltoast.com>
2025-01-18 22:56:31 -0800

    max77779_charger: Remove unused trig_type field to fix shift-out-of-bounds
    UBSAN generates the following splat:
    [    1.380673] ================================================================================
    [    1.380688] UBSAN: shift-out-of-bounds in ../google-modules/bms/max77779_charger.c:3464:27
    [    1.380699] shift exponent 48 is too large for 32-bit type 'int'
    [    1.380715] CPU: 5 PID: 103 Comm: kworker/u16:3 Tainted: G S                 6.1.84-Sultan #1
    [    1.380728] Hardware name: ZUMA PRO CAIMAN MP board based on ZUMA PRO (DT)
    [    1.380735] Workqueue: events_unbound deferred_probe_work_func
    [    1.380780] Call trace:
    [    1.380788] show_stack+0xa4/0xb0
    [    1.380819] dump_stack_lvl+0x68/0x80
    [    1.380859] dump_stack+0x18/0x30
    [    1.380866] ubsan_epilogue+0x10/0x40
    [    1.380936] __ubsan_handle_shift_out_of_bounds+0xd0/0x170
    [    1.380968] max77779_chg_set_irq_type+0xd0/0xe0
    [    1.381047] __irq_set_trigger+0x48/0x1b4
    [    1.381069] __setup_irq+0x3c4/0x7c0
    [    1.381076] request_threaded_irq+0xc8/0x1a0
    [    1.381084] devm_request_threaded_irq+0x80/0xf4
    [    1.381109] google_cpm_probe+0x674/0xbd4
    [    1.381122] platform_probe+0x68/0x100
    [    1.381149] really_probe+0xc8/0x390
    [    1.381158] __driver_probe_device+0x84/0x150
    [    1.381164] driver_probe_device+0x38/0x120
    [    1.381171] __device_attach_driver+0x8c/0x150
    [    1.381178] bus_for_each_drv+0x80/0xd8
    [    1.381248] device_initial_probe+0xf8/0x1ac
    [    1.381255] bus_probe_device+0xac/0xb4
    [    1.381266] deferred_probe_work_func+0x90/0xc4
    [    1.381272] process_one_work+0x1b0/0x31c
    [    1.381320] worker_thread+0x68/0x4a0
    [    1.381329] kthread+0xf4/0x100
    [    1.381351] ret_from_fork+0x10/0x20
    [    1.381364] ================================================================================
    [    1.381370] ================================================================================
    [    1.381375] UBSAN: shift-out-of-bounds in ../google-modules/bms/max77779_charger.c:3465:28
    [    1.381383] shift exponent 48 is too large for 32-bit type 'unsigned int'
    [    1.381390] CPU: 5 PID: 103 Comm: kworker/u16:3 Tainted: G S                 6.1.84-Sultan #1
    [    1.381398] Hardware name: ZUMA PRO CAIMAN MP board based on ZUMA PRO (DT)
    [    1.381404] Workqueue: events_unbound deferred_probe_work_func
    [    1.381412] Call trace:
    [    1.381416] show_stack+0xa4/0xb0
    [    1.381426] dump_stack_lvl+0x68/0x80
    [    1.381434] dump_stack+0x18/0x30
    [    1.381442] ubsan_epilogue+0x10/0x40
    [    1.381452] __ubsan_handle_shift_out_of_bounds+0xd0/0x170
    [    1.381460] max77779_chg_set_irq_type+0xb4/0xe0
    [    1.381469] __irq_set_trigger+0x48/0x1b4
    [    1.381476] __setup_irq+0x3c4/0x7c0
    [    1.381484] request_threaded_irq+0xc8/0x1a0
    [    1.381491] devm_request_threaded_irq+0x80/0xf4
    [    1.381500] google_cpm_probe+0x674/0xbd4
    [    1.381507] platform_probe+0x68/0x100
    [    1.381514] really_probe+0xc8/0x390
    [    1.381521] __driver_probe_device+0x84/0x150
    [    1.381528] driver_probe_device+0x38/0x120
    [    1.381534] __device_attach_driver+0x8c/0x150
    [    1.381541] bus_for_each_drv+0x80/0xd8
    [    1.381550] device_initial_probe+0xf8/0x1ac
    [    1.381557] bus_probe_device+0xac/0xb4
    [    1.381567] deferred_probe_work_func+0x90/0xc4
    [    1.381574] process_one_work+0x1b0/0x31c
    [    1.381583] worker_thread+0x68/0x4a0
    [    1.381591] kthread+0xf4/0x100
    [    1.381598] ret_from_fork+0x10/0x20
    [    1.381606] ================================================================================
    
    Since the trig_type field isn't used anywhere, just remove it to fix the
    out-of-bounds bit shift.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/bms/max77779_charger.c
+++ google-modules/bms/max77779_charger.c
@@ -3517,22 +3517,16 @@ static void max77779_chg_irq_enable(struct irq_data *d)
 
 static int max77779_chg_set_irq_type(struct irq_data *d, unsigned int type)
 {
-	struct max77779_chgr_data *data = irq_data_get_irq_chip_data(d);
-
 	switch (type) {
 	case IRQF_TRIGGER_NONE:
 	case IRQF_TRIGGER_RISING:
 	case IRQF_TRIGGER_FALLING:
 	case IRQF_TRIGGER_HIGH:
 	case IRQF_TRIGGER_LOW:
-		data->trig_type &= (0xf << (d->hwirq * 4));
-		data->trig_type |= (type << (d->hwirq * 4));
-		break;
-	default:
-		return -EINVAL;
+		return 0;
 	}
 
-	return 0;
+	return -EINVAL;
 }
 
 static void max77779_chg_bus_lock(struct irq_data *d)
