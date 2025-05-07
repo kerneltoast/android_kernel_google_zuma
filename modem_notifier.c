c488359e9abdc418372dcec3400b8f4b173481ff
Sultan Alsawaf <sultan@kerneltoast.com>
2025-05-06 18:05:46 -0700

    Squashed 'google-modules/radio/samsung/s5300/' content from commit 9b16eed83890
    git-subtree-dir: google-modules/radio/samsung/s5300
    git-subtree-split: 9b16eed8389000007beeda150acdea975521e1f7

---
+++ modem_notifier.c
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2015 Samsung Electronics.
+ *
+ */
+
+#include <linux/stdarg.h>
+#include <linux/string.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/notifier.h>
+#include "modem_notifier.h"
+
+#include "modem_prj.h"
+#include "modem_utils.h"
+
+static struct raw_notifier_head modem_event_notifier;
+#if IS_ENABLED(CONFIG_CPIF_AP_SUSPEND_DURING_VOICE_CALL)
+static struct raw_notifier_head modem_voice_call_event_notifier;
+#endif
+
+int register_modem_event_notifier(struct notifier_block *nb)
+{
+	if (!nb)
+		return -ENOENT;
+
+	return raw_notifier_chain_register(&modem_event_notifier, nb);
+}
+EXPORT_SYMBOL(register_modem_event_notifier);
+
+void modem_notify_event(enum modem_event evt, void *mc)
+{
+	/* ToDo */
+}
+EXPORT_SYMBOL(modem_notify_event);
+
+#if IS_ENABLED(CONFIG_CPIF_AP_SUSPEND_DURING_VOICE_CALL)
+int register_modem_voice_call_event_notifier(struct notifier_block *nb)
+{
+	if (!nb)
+		return -ENOENT;
+	return raw_notifier_chain_register(&modem_voice_call_event_notifier, nb);
+}
+
+void unregister_modem_voice_call_event_notifier(struct notifier_block *nb)
+{
+	if (nb)
+		raw_notifier_chain_unregister(&modem_voice_call_event_notifier, nb);
+}
+
+void modem_voice_call_notify_event(enum modem_voice_call_event evt, void *data)
+{
+	mif_info("voice call event notify (%d) ++\n", evt);
+	raw_notifier_call_chain(&modem_voice_call_event_notifier, evt, data);
+	mif_info("voice call event notify (%d) --\n", evt);
+}
+EXPORT_SYMBOL(modem_voice_call_notify_event);
+#endif
