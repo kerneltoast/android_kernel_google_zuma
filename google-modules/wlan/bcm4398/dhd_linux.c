0af6592a078d35185e9df902ebc35293bc75f4b5
Sultan Alsawaf <sultan@kerneltoast.com>
2025-06-14 20:56:46 -0700

    bcmdhd: Fix memset length for dhd->monitor_type in dhd_del_monitor_if()
    GCC warns:
    ../google-modules/wlan/bcm4390/dhd_linux.c: In function 'dhd_del_monitor_if':
    ../google-modules/wlan/bcm4390/dhd_linux.c:5873:9: error: 'memset' used with length equal to number of elements without multiplication by element size [-Werror=memset-elt-size]
     5873 |         memset(dhd->monitor_type, 0, DHD_MAX_IFS);
          |         ^~~~~~
    
    `monitor_type` is a uint array, and uint is not 1 byte, so the memset
    length is incorrect. Fix it by using sizeof() on dhd->monitor_type itself.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/wlan/bcm4398/dhd_linux.c
+++ google-modules/wlan/bcm4398/dhd_linux.c
@@ -5875,7 +5875,7 @@ dhd_del_monitor_if(dhd_info_t *dhd)
 	dev_priv = DHD_MON_DEV_PRIV(dhd->monitor_dev);
 	dev_priv->dhd = (dhd_info_t *)NULL;
 	memset(&dev_priv->stats, 0, sizeof(dev_priv->stats));
-	memset(dhd->monitor_type, 0, DHD_MAX_IFS);
+	memset(dhd->monitor_type, 0, sizeof(dhd->monitor_type));
 #endif /* WL_CFG80211_MONITOR */
 
 	if (FW_SUPPORTED((&dhd->pub), monitor)) {
